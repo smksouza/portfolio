2873d8ca64f527e60ce3957833d38fda1cab9590
Samuel Souza <smksouza.dev@gmail.com>
2023-08-20 20:56:12 -0300

    criação do efeito de revelar com scroll

--- js/script.js
+++ js/script.js
@@ -1,38 +1,48 @@
-const btn = document.getElementById("btnTop");
-const btnCopyEmail = document.getElementById("copyEmail");
-const rocketNotesOverlay = document.getElementById("Rocketnotes");
-const projectsInfoSection = document.getElementById("projectsInfo");
-const btnCloseRocket = document.getElementById("closeRocket");
+function handleScrollButton() {
+  const btn = document.getElementById("btnTop");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  });
 
-const email = "smksouza.dev@gmail.com";
+  btn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+}
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 200) {
-    btn.style.display = "block";
-  } else {
-    btn.style.display = "none";
-  }
-});
+function handleCopyEmail() {
+  const email = "smksouza.dev@gmail.com";
+  const btnCopyEmail = document.getElementById("copyEmail");
 
-btn.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
+  btnCopyEmail.addEventListener("click", () => {
+    navigator.clipboard
+      .writeText(email)
+      .then(() => {
+        alert("Email copiado com sucesso !");
+      })
+      .catch((error) => {
+        console.error("Error ao copiar o email:", error);
+      });
+  });
+}
 
-btnCopyEmail.addEventListener("click", () => {
-  navigator.clipboard
-    .writeText(email)
-    .then(() => {
-      alert("Email copiado com sucesso !");
-    })
-    .catch((error) => {
-      console.error("Error ao copiar o email:", error);
-    });
-});
+function handleRocketNotesOverlay() {
+  const rocketNotesOverlay = document.getElementById("Rocketnotes");
+  const projectsInfoSection = document.getElementById("projectsInfo");
+  const btnCloseRocket = document.getElementById("closeRocket");
 
-rocketNotesOverlay.addEventListener("click", () => {
-  projectsInfoSection.style.display = "grid";
-});
+  rocketNotesOverlay.addEventListener("click", () => {
+    projectsInfoSection.style.display = "grid";
+  });
 
-btnCloseRocket.addEventListener("click", () => {
-  projectsInfoSection.style.display = "none";
-});
+  btnCloseRocket.addEventListener("click", () => {
+    projectsInfoSection.style.display = "none";
+  });
+}
+
+handleCopyEmail();
+handleScrollButton();
+handleRocketNotesOverlay();
---
+++ js/scrollReveal.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({ reset: true });
+sr.reveal(".homeReveal", { duration: 2500 });
+sr.reveal(".about-title", { duration: 2500 });
+sr.reveal(".about-me", { duration: 2500 });
+sr.reveal(".about-img", { duration: 2500 });
+sr.reveal(".my-technologies", { duration: 2500 });
+sr.reveal(".projectsTitle", { duration: 2500 });
+sr.reveal(".myProjects", { duration: 2500 });
