604b51aa49f5ad6c96d523753ac92a02abc6452a
Samuel Souza <smksouza.dev@gmail.com>
2023-08-23 09:55:42 -0300

    projetos responsivos

--- js/script.js
+++ js/script.js
@@ -43,6 +43,19 @@ function handleRocketNotesOverlay() {
   });
 }
 
+function handleReadMoreButton() {
+  const readBtn = document.getElementById("readBtn");
+  const myProjects = document.querySelector(".myProjects");
+  readBtn.addEventListener("click", () => {
+    myProjects.classList.toggle("active");
+    if (myProjects.classList.contains("active")) {
+      return (readBtn.textContent = "Ver menos");
+    }
+    readBtn.textContent = "Ver mais";
+  });
+}
+
+handleReadMoreButton();
 handleCopyEmail();
 handleScrollButton();
 handleRocketNotesOverlay();
