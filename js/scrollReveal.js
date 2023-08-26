9e44a16494b66b31ded2160fd75bb3616ecfdcd7
Samuel Souza <smksouza.dev@gmail.com>
2023-08-25 22:54:59 -0300

    responsividade projetos:overlay

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -4,5 +4,6 @@ sr.reveal(".about-title", { duration: 2500 });
 sr.reveal(".about-me", { duration: 2500 });
 sr.reveal(".about-img", { duration: 2500 });
 sr.reveal(".my-technologies", { duration: 2500 });
+sr.reveal(".technologies", { duration: 2500 });
 sr.reveal(".projectsTitle", { duration: 2500 });
 sr.reveal(".myProjects", { duration: 2500 });
