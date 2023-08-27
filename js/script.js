e6acdc36a1b468f8a680b67689a7a5afae8fba0e
Samuel Souza <smksouza.dev@gmail.com>
2023-08-27 14:13:09 -0300

    responsividade moba

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 import { handleReadMoreButton } from "./readMoreButton.js";
+import { showProjectsInfo } from "./showProjectsInfo.js";
 import { handleScrollButton } from "./scrollButton.js";
 import { handleCopyEmail } from "./copyEmail.js";
 import { reveal } from "./scrollReveal.js";
-import { showProjectsInfo } from "./showProjectsInfo.js";
 
-showProjectsInfo();
 reveal();
-handleReadMoreButton();
 handleCopyEmail();
+showProjectsInfo();
 handleScrollButton();
-
+handleReadMoreButton();
